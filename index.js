57b784929b6d7ecb614e05a710f47a0c453518ff
David Halls <dahalls@gmail.com>
2017-12-17 08:58:09 +0000

    Initial commit. WIP!

---
+++ index.js
@@ -0,0 +1,240 @@
+// TODO
+// Migration produces empty DB, do we just copy that for a project?
+// Jobs are just string IDs. Should we have a job table that's extensible?
+// Gruntfile, tests, docs
+
+const crypto = require('crypto'),
+      sqlite3 = require('sqlite3'),
+      async = require('async'),
+      EventEmitter = require('events').EventEmitter;
+
+class Atributo extends EventEmitter
+{
+    constructor(options)
+    {
+        this._db = new sqlite3.Database(options.db_filename);
+        this._db.on('error', this.emit.bind(this, 'error'));
+        this._db.on('open', this.emit.bind(this, 'ready'));
+
+        // We need to queue queries on a db connection:
+        // https://github.com/mapbox/node-sqlite3/issues/304
+        // Alternative would be to create a separate connection
+        // for each query. The calling application can still do
+        // this if required (to achieve more parallelism) by
+        // creating many Atributo objects.
+        this._queue = async.queue((task, cb) => task(cb));
+    }
+
+    close(cb)
+    {
+        this._db.close(cb);
+    }
+
+    _end_transaction(cb, err, ...args)
+    {
+        if (err)
+        {
+            return this._queue.push(cb =>
+            {
+                this._db.run('ROLLBACK',
+                             cb);
+            }, err2 => cb(err2 || err, ..args);
+        }
+
+        this._queue.push(cb =>
+        {
+            this._db.run('END TRANSACTION',
+                         cb);
+        }, err => cb(err, ...args));
+    }
+        
+    available(instance_id, cb)
+    {
+        this._queue.push(cb => async.waterfall(
+        [
+            cb =>
+            {
+                // Avoid SQLITE_BUSY_SNAPSHOT errors by starting the transaction
+                // immediately so other transactions can't start. We can do this
+                // because we know in this case that we're definitely going to
+                // update the database.
+                this._db.run('BEGIN IMMEDIATE TRANSACTION',
+                             cb);
+            },
+            cb =>
+            {
+                this._db.run('INSERT OR IGNORE INTO instances VALUES (?, 1);',
+                             instance_id,
+                             cb);
+            },
+            cb =>
+            {
+                // Do this in a transaction so we don't error if someone else
+                // deletes the row here.
+                this._db.run('UPDATE instances SET available = 1 WHERE id = ?;',
+                             instance_id,
+                             cb);
+            }
+        ], cb), this._end_transaction.bind(this, cb));
+    }
+
+    unavailable(instance_id, destroyed, cb)
+    {
+        let queries = [
+            cb =>
+            {
+                this._db.run('BEGIN IMMEDIATE TRANSACTION',
+                             cb);
+            },
+            cb =>
+            {
+                this._db.run('INSERT OR IGNORE INTO instances VALUES (?, 0);',
+                             instance_id,
+                             cb);
+            },
+            cb =>
+            {
+                this._db.run('UPDATE instances SET available = 0 WHERE id = ?;',
+                             instance_id,
+                             cb);
+            }
+        ];
+
+        if (destroyed)
+        {
+            queries.push(
+                cb =>
+                {
+                    this._db.run('DELETE FROM allocations WHERE instance = ?;',
+                                 instance_id,
+                                 cb);
+                },
+                cb =>
+                {
+                    this._db.run('DELETE FROM instances WHERE id = ?;',
+                                 instance_id,
+                                 cb);
+                }
+            );
+        }
+
+        this._queue.push(cb => async.waterfall(queries, cb),
+                         this._end_transaction.bind(this, cb));
+    }
+
+    has_no_jobs(instance_id, cb)
+    {
+        this._queue.push(cb =>
+        {
+            this._db.get('SELECT count(*) FROM allocations WHERE instance = ?;',
+                         instance_id,
+                         cb);
+        }, cb);
+    }
+
+    allocate(job_id, options, cb)
+    {
+        if (typeof options === 'function')
+        {
+            cb = options;
+            options = null;
+        }
+
+        options = Object.assign(
+        {
+            allocator: Atributo.default_allocator
+        }, options);
+
+        this._queue.push(cb => async.waterfall(
+        [
+            cb =>
+            {
+                this._db.run('BEGIN TRANSACTION',
+                             cb);
+            },
+            cb =>
+            {
+                this._db.get('SELECT instance FROM allocations WHERE job = ?;',
+                             job_id,
+                             cb);
+            }
+        ], cb), (err, instance_ids) =>
+        {
+            if (err)
+            {
+                return this._end_transaction(cb, err);
+            }
+
+            if (instance_ids.length > 0)
+            {
+                return this._end_transaction(cb, null, false, instance_id[0]);
+            }
+
+            this._queue.push(cb => 
+            {
+                this._db.get('SELECT id FROM instances WHERE available = 1;',
+                             cb);
+            }, (err, instance_ids) =>
+            {
+                if (err)
+                {
+                    return this._end_transaction(cb, err);
+                }
+
+                if (instance_ids.length === 0)
+                {
+                    return this._end_transaction(cb, new Error('no instances'));
+                }
+
+                options.allocator.call(this, job_id, instance_ids, (err, allocate, instance_id) =>
+                {
+                    if (err)
+                    {
+                        return this._end_transaction(cb, err);
+                    }
+
+                    if (!allocate)
+                    {
+                        return this._end_transaction(cb, null, false, instance_id);
+                    }
+
+                    this._queue.push(cb =>
+                    {
+                        this._db.run('INSERT INTO allocations VALUES (?, ?);',
+                                     job_id,
+                                     instance_id,
+                                     cb);
+                    }, err =>
+                    {
+                        if (err)
+                        {
+                            return this._end_transaction(cb, err);
+                        }
+
+                        this._end_transaction(cb, null, true, instance_id);
+                    });
+                });
+            });
+        });
+    }
+
+    deallocate(job_id, cb)
+    {
+        this._queue.push(cb ->
+        {
+            this._db.run('DELETE FROM allocations WHERE job = ?;',
+                         job_id,
+                         cb);
+        }, cb);
+    }
+}
+
+Atributo.default_allocator = function (job_id, instance_ids, cb)
+{
+    let h = crypto.createHash('md5'); // not for security, just mapping
+    h.update(job_id);
+    let buf = h.digest();
+    cb(null, instance_ids[buf.readUInt32BE(0) % instance_ids.length]);
+};
+
+exports.Atributo = Atributo;
---
+++ migrations/1513325138829-initial.js
@@ -0,0 +1,55 @@
+'use strict'
+
+const sqlite3 = require('sqlite3'),
+      async = require('async'),
+      db_filename = 'atributo.empty.sqlite3';
+
+function with_db(f, cb)
+{
+    let db = new sqlite3.Database(db_filename);
+    db.on('error', cb);
+    db.on('open', f);
+}
+
+module.exports.up = function (next)
+{
+    with_db(function ()
+    {
+        async.series(
+        [
+            cb =>
+            {
+                this.run('CREATE TABLE instances (' +
+                         '  id TEXT PRIMARY KEY,' +
+                         '  available BOOLEAN);',
+                         cb);
+            },
+            cb =>
+            {
+                this.run('CREATE TABLE allocations (' +
+                         '  job TEXT UNIQUE,' +
+                         '  instance TEXT UNIQUE,' +
+                         '  FOREIGN KEY(instance) REFERENCES instances(id));',
+                         cb);
+            }
+        ], next);
+    }, next);
+};
+
+module.exports.down = function (next)
+{
+    with_db(function ()
+    {
+        async.series(
+        [
+            cb =>
+            {
+                this.run('DROP TABLE instances;', cb);
+            },
+            cb =>
+            {
+                this.run('DROP TABLE allocations;', cb);
+            }
+        ], next);
+    }, next);
+};
