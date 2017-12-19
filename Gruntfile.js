31d2e012eebcbad47c5da7937c1f506af03eb1b2
David Halls <dahalls@gmail.com>
2017-12-19 10:50:25 +0000

    Run coverage

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,19 @@
+const path = require('path'),
+      mod_path = path.join('.', 'node_modules'),
+      bin_path = path.join(mod_path, '.bin'),
+      nyc_path = path.join(bin_path, 'nyc');
+
+let grunt_path;
+
+if (process.platform === 'win32')
+{
+    grunt_path = path.join(mod_path, 'grunt', 'bin', 'grunt');
+}
+else
+{
+    grunt_path = path.join(bin_path, 'grunt');
+}
+
 module.exports = function (grunt)
 {
     grunt.initConfig(
@@ -18,6 +34,24 @@ module.exports = function (grunt)
                 src: 'atributo.empty.sqlite3',
                 dest: 'test/atributo.sqlite3'
             }
+        },
+
+        exec: {
+            cover: {
+                cmd: nyc_path + " -x Gruntfile.js -x \"" + path.join('test', '**') + "\" node " + grunt_path + " test"
+            },
+
+            cover_report: {
+                cmd: nyc_path + ' report -r lcov'
+            },
+
+            cover_check: {
+                cmd: nyc_path + ' check coverage --statements 100 --branches 100 --functions 100 --lines 100'
+            },
+
+            coveralls: {
+                cmd: 'cat coverage/lcov.info | ./node_modules/.bin/coveralls'
+            }
         }
     });
 
@@ -29,5 +63,9 @@ module.exports = function (grunt)
     grunt.registerTask('lint', 'eslint');
     grunt.registerTask('test', ['copy:db',
                                 'mochaTest']);
-
+    grunt.registerTask('coverage', ['exec:cover',
+                                    'exec:cover_report',
+                                    'exec:cover_check']);
+    grunt.registerTask('coveralls', 'exec:coveralls');
+    grunt.registerTask('default', ['lint', 'test']);
 };
